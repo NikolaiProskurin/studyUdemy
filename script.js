146ea264c336d3814df1101fce459b9b0b1e3634
Nikolai Proskurin <nikoworkpin@gmail.com>
2022-07-16 15:17:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// console.log(1);
+
+// const arr = [1, 2, 3, 5, 6, 6, 8, 9, 8, 8];
+
+// const arrObj = {
+//   name: "Diana",
+//   age: 88,
+//   lvl: 80,
+//   skills: {
+//     speed: 10,
+//     abc: {
+//       defens: {
+//         lol: 10,
+//       },
+//     },
+//   },
+// };
+
+// console.log(arr[1]);
+
+// const duplic = arr => [...arr, ...arr];
+// duplic([1,2,3,4,5]);
+
+// const companiName = "YOLO";
+// let aboutYolo = {
+//   sience: 1995,
+//   director: "Nch",
+//   anotherPeopleOfCompani: ["Alex", "Yra", "Serj"],
+//   productCompani: {
+//     theSystem: 100,
+//     theWorld: 100,
+//   },
+//   teers: 200,
+// };
+
+// console.log(aboutYolo.anotherPeopleOfCompani[2]);
+
+// alert("GO, HOME");
+
+// const answers = [];
+
+// const result = confirm("you ok?");
+// answers[0] = prompt("dont lie, write what about you!");
+// answers[1] = prompt("What your name?");
+// answers[2] = prompt("How are you old?");
+// answers[3] = prompt("What you like?");
+
+// document.write(answers);
